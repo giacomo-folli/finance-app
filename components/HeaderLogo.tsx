0b857d274831e45820603e24b58ad15ba6bf852c
Giacomo Folli <119410424+giacomo-folli@users.noreply.github.com>
2024-09-15 23:13:55 +0200

    before adding db

---
+++ components/HeaderLogo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export const HeaderLogo = () => {
+  return (
+    <Link href="/">
+      <div className="items-center hidden lg:flex">
+        <Image src="/logo.svg" width={28} height={28} alt="logo" />
+        <p className="font-semibold text-white text-2xl ml-2.5">Finance</p>
+      </div>
+    </Link>
+  );
+};
